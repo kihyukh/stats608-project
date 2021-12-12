db866054fc41d977f9f4290e1f2cd602e85342c3
Kihyuk Hong <kihyukh@umich.edu>
2021-12-12 10:37:09 -0500

    more demos

--- make_plot.py
+++ make_plot.py
@@ -7,6 +7,7 @@ from matplotlib.pyplot import figure
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--log')
 parser.add_argument('--label')
+parser.add_argument('--vertical')
 
 args = parser.parse_args()
 
@@ -25,6 +26,9 @@ for i, filename in enumerate(args.log.split(',')):
             ts.append(int(t))
             regret.append(float(r) / int(t))
     sns.lineplot(x=ts, y=regret, label=labels[i] if labels else '')
+
+if args.vertical:
+    plt.axvline(x=float(args.vertical), linestyle='--', color='r')
 
 plt.xlabel('time')
 plt.ylabel('average regret')
